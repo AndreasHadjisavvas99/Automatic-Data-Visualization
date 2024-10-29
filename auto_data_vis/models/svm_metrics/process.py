0a6f4ea9f494c207d23a021178a76ae0bb8009a0
Andreas Hadjisavvas <93579450+AndreasHadjisavvas99@users.noreply.github.com>
2024-10-29 17:02:17 +0200

    Add files via upload

---
+++ auto_data_vis/models/svm_metrics/process.py
@@ -0,0 +1,58 @@
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import pandas as pd
+import numpy as np
+
+def preprocess_data(df, apply_percentile_cutoff=True, apply_scaling=True, impute_missing=True, apply_one_hot_encoding=True):
+    X_categorical = pd.DataFrame()
+    X_numerical = pd.DataFrame()
+    X_boolean = pd.DataFrame()
+
+    # Splitting data to categorical and numerical for later purposes
+    for column in df.columns:
+        if df[column].dtype == 'object':
+            X_categorical[column] = df[column]
+        elif df[column].dtype == 'bool':
+            X_boolean[column] = df[column].astype(int)
+        else:
+            X_numerical[column] = df[column]
+
+    # 1) Set numerical features above the 99th percentile or below the 1st percentile to those respective cut-offs
+    if apply_percentile_cutoff:
+        #print("Know applying percentile cutoffs")
+        percentile_1 = X_numerical.quantile(0.01)
+        percentile_99 = X_numerical.quantile(0.99)
+        for column in X_numerical.columns:
+            X_numerical[column] = np.where(X_numerical[column] > percentile_99[column], percentile_99[column], X_numerical[column])
+            X_numerical[column] = np.where(X_numerical[column] < percentile_1[column], percentile_1[column], X_numerical[column])
+
+    # 2) Remove the mean of numeric fields and scale to unit variance
+    if apply_scaling:
+        #print("Know Scaling data")
+        scaler = StandardScaler()
+        numeric_data = X_numerical.select_dtypes(include=['float64', 'int64'])
+        X_numerical[numeric_data.columns] = scaler.fit_transform(numeric_data)
+
+    # 3a) Impute missing categorical values using the mode of non-missing values
+    if impute_missing:
+        #print("Know applying imputation to missing data")
+        for column in X_categorical.columns:
+            mode_val = X_categorical[column].mode()[0]
+            X_categorical[column].fillna(mode_val, inplace=True)
+
+        # 3b) Impute missing numerical values with the mean of non-missing values
+        for column in X_numerical.columns:
+            mean_val = X_numerical[column].mean()
+            X_numerical[column].fillna(mean_val, inplace=True)
+
+    # 4) One-hot encoding to categorical features
+    if apply_one_hot_encoding:
+        #print("Know One-Hot Encoding")
+        one_hot_encoder = OneHotEncoder()
+        X_categorical_encoded = one_hot_encoder.fit_transform(X_categorical).toarray()
+        encoded_feature_names = one_hot_encoder.get_feature_names_out(X_categorical.columns)
+        X_categorical = pd.DataFrame(X_categorical_encoded, columns=encoded_feature_names)
+
+    # Concatenate all the processed features
+    X = pd.concat([X_numerical, X_categorical, X_boolean], axis=1)
+    
+    return X
---
+++ auto_data_vis/models/svm_metrics/svm_model.py
@@ -0,0 +1,64 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, GridSearchCV
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from process import preprocess_data
+import time
+import os
+
+# Define datasets
+datasets = {
+    #'c2_sp': '../../features1/c2_sp.csv',
+    #'c2_sb': '../../features1/c2_sb.csv',
+    #'c2_ls': '../../features1/c2_ls.csv',
+    #'c2_lp': '../../features1/c2_lp.csv',
+    #'c2_lb': '../../features1/c2_lb.csv',
+    #'c2_bp': '../../features1/c2_bp.csv',
+    #'c3_lsp': '../../features1/c3_lsp.csv',
+    #'c3_sbp': '../../features1/c3_sbp.csv',
+    #'c3_lbp': '../../features1/c3_lbp.csv',
+    #'c3_lsb': '../../features1/c3_lsb.csv',
+    'c4_lsbp': '../../features1/c4_lsbp.csv'
+}
+
+for name, file_path in datasets.items():
+    print(f"Processing dataset: {name}")
+
+    # Load dataset
+    df = pd.read_csv(file_path)
+    df = df.rename(columns={'Plot_Type': 'Label'})
+
+    # Prepare features and labels
+    feature_names = df.drop('Label', axis=1).columns
+    y = df["Label"].values
+    X = df.drop(labels=['Label'], axis=1)
+
+    # Preprocess data
+    X = preprocess_data(X, apply_percentile_cutoff=True, apply_scaling=True, impute_missing=True, apply_one_hot_encoding=True)
+
+    # Split data into training and test sets
+    X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.2, random_state=42)
+
+    hyperparameters = {
+        'C': 10, 
+        'kernel': 'rbf',
+        'gamma': 'auto'
+    }
+
+    svm_classifier = SVC(**hyperparameters)
+
+    start_time = time.time()
+    svm_classifier.fit(X_train, y_train)
+    training_duration = time.time() - start_time
+
+    # Evaluate model
+    val_predictions =svm_classifier.predict(X_val)
+    val_accuracy = accuracy_score(y_val, val_predictions)
+    
+    # Evaluate the best model on the test set
+    test_predictions = svm_classifier.predict(X_test)
+    test_accuracy = accuracy_score(y_test, test_predictions)
+
+    print(f"Validation accuracy for {name}: {val_accuracy:.4f}")
+    print(f"Training duration: {training_duration:.2f} seconds")
